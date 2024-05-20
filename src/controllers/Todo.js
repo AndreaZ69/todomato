e962dc75421769de1d0276cc5eedaff76a45cd9f
Emanuele Fava <manuelfavaa@gmail.com>
2024-05-20 13:39:43 +0200

    feat(emanuele_fava): modifica e aggiunta files todo

--- src/controllers/Todo.js
+++ src/controllers/Todo.js
@@ -1,1 +1,49 @@
-class ControllerTodo {}
+const ModelUser = require("../models/Todo.js");
+
+class ControllerTodo {
+  #todo = [];
+
+  addTodo(nomeAttivita, description) {
+    const todo = new ModelTodo(nomeAttivita, description);
+    this.#todo.push(todo);
+  }
+
+  readTodo(todoId) {
+    const todoFound = this.#todo.find(function (element) {
+      if (element.todoId === todoId) {
+        return todoFound;
+      } else return false;
+    });
+  }
+
+  updateTodoName(todoId, newName) {
+    this.#todo = this.#todo.map(function (element) {
+      if (element.todoId === todoId) {
+        element.nomeAttivita = newName;
+      }
+    });
+  }
+
+  updateTodoDescription(todoId, newDescription) {
+    this.#todo = this.#todo.map(function (element) {
+      if (element.todoId === todoId) {
+        element.description = newDescription;
+      }
+    });
+  }
+  deleteTodo(todoId) {
+    this.#todo = this.#todo.filter(function (element) {
+      if (element.todoId === todoId) {
+        return true;
+      } else return false;
+    });
+  }
+
+  getTodo(todoId) {
+    const todoGot = this.#todo.find(function (element) {
+      if (todoId === todo.todoId) {
+        return todoGot;
+      } else return false;
+    });
+  }
+}
--- src/models/Todo.js
+++ src/models/Todo.js
@@ -1,10 +1,10 @@
 class ModelTodo {
   constructor(nomeAttivita, description) {
+    this.todoId = Math.random();
     this.nomeAttivita = nomeAttivita;
     this.description = description;
     this.date = new Date();
-    this.timer = new Timer(nomeAttivita, date, timer, durata);
-
-    //da agg poi giorno x avrai una determinata attività da svolgere
   }
 }
+
+module.exports = ModelTodo;
