6750a15ee425125950b653fe4d8c8300b5388791
Alessio Di Noto <alessio.dinoto@outlook.it>
2024-06-07 12:44:51 +0200

    feat(alessio_di_noto): fix login e register e separazione file js

---
+++ src/login.js
@@ -0,0 +1,28 @@
+/** @format */
+
+import { ControllerUsers } from './controllers/Users.js';
+
+const userController = new ControllerUsers();
+
+document.getElementById('loginForm')?.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const username = document.getElementById('loginUsername').value;
+  const email = document.getElementById('loginEmail').value;
+  const password = document.getElementById('loginPassword').value;
+
+  let user = null;
+  if (username) {
+    user = userController.getUsername(username, password);
+  } else if (email) {
+    user = userController.getEmail(email, password);
+  }
+
+  if (user) {
+    document.getElementById('loginMessage').innerText = 'Login successful!';
+    setTimeout(() => {
+      window.location.href = 'todomato.html';
+    }, 2000);
+  } else {
+    document.getElementById('loginMessage').innerText = 'Invalid credentials';
+  }
+});
