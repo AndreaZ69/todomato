5a0194db6fade100cc563e577c9ff5a63ce97f98
Emanuele Fava <manuelfavaa@gmail.com>
2024-06-18 22:17:30 +0200

    feat(emanuele_fava): messaggio utente loggato e correzioni css

--- src/index.js
+++ src/index.js
@@ -4,15 +4,24 @@ const buttonLogIn = document.getElementById('btnLogIn');
 const buttonLogOn = document.getElementById('btnLogOn');
 const buttonToDoMato = document.getElementById('btnToDoMato');
 const nav = document.getElementById('indexNav');
-let currentUser = localStorage.getItem('userLogged');
 
 document.addEventListener('DOMContentLoaded', () => {
+  let currentUser = localStorage.getItem('userLogged');
+
   if (!!currentUser) {
     buttonLogIn.style.display = 'none';
     buttonLogOn.style.display = 'none';
     buttonToDoMato.style.display = 'block';
+    let objectUser = JSON.parse(currentUser);
+    let loggedUserName = objectUser.username;
+    const loggedUserNamep = document.createElement('p');
+    loggedUserNamep.classList.add('loggedUserText');
+
+    loggedUserNamep.innerHTML = 'Bentornato utente <b>' + loggedUserName + '</b>!';
+    nav.appendChild(loggedUserNamep);
 
     const logOutButton = document.createElement('button');
+    logOutButton.classList.add('btnLogIn');
     logOutButton.textContent = 'LogOut';
     nav.appendChild(logOutButton);
 
@@ -22,10 +31,11 @@ document.addEventListener('DOMContentLoaded', () => {
       buttonLogIn.style.display = 'block';
       buttonLogOn.style.display = 'block';
       buttonToDoMato.style.display = 'none';
+      loggedUserNamep.style.display = 'none';
     });
   } else {
-    logOutButton.style.display = 'none';
     buttonLogIn.style.display = 'block';
-    //rimuovere il logout e login nel caso in cui non si è loggati o loggati
+    buttonLogOn.style.display = 'block';
+    buttonToDoMato.style.display = 'none';
   }
 });
